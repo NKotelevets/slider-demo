8025f544f0cf9bb6db62cd9a339419c4c9bfbe34
Nikolay-Kotelevets <nikolay.kotelevets@scrumlaunch.com>
2023-04-27 21:42:23 +0300

    new version

--- src/components/swiper/SwiperComponent.jsx
+++ src/components/swiper/SwiperComponent.jsx
@@ -48,37 +48,62 @@ export function SwiperComponent() {
         slidesPerView={1}
         className="mySwiper"
         effect={"fade"}
+        loop={true}
         modules={[Pagination, EffectFade, Keyboard, Mousewheel]}
       >
         <SwiperSlide className="swiper-slide slide1">
           <div className="slide-wrapper1">
             <div className="slide-content">
-              <p>Hello.</p>
-              <p>
-                {" "}
-                LeftFront is a <strong>societal accelerator.</strong>
-              </p>
-              <p>
-                We shape the future of our economy and culture in real time.
-              </p>
+              <p className="slide-title">Hello.</p>
+              <div className="text-wrapper">
+                <p className="slide-text">
+                  {" "}
+                  LeftFront is a{" "}
+                  <span className="font-medium">societal accelerator.</span>
+                </p>
+                <p className="slide-text">
+                  We shape the future of our economy{" "}
+                  <span className="show-for-web">and</span>{" "}
+                  <span className="show-for-mob">+ </span>culture <br />
+                  in real time.
+                </p>
+              </div>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide className="swiper-slide slide2">
           <div className="slide-wrapper2">
             {" "}
-            <div className="slide-content">
+            <div className="slide-content show-for-web">
               <p>
                 {" "}
-                Established by a group of expert executives with a remarkable
-                record as pioneers, operators, creators, artists and academics,
-                LeftFront undertakes Daedalian problems with a rare level of
-                proficiency, integrity + efficacy.{" "}
+                Established by a group of expert executives with a <br />{" "}
+                remarkable record as pioneers, operators, creators,
+                <br /> artists and academics, LeftFront undertakes <br />{" "}
+                Daedalian problems with a rare level of proficiency,
+                <br /> integrity + efficacy.{" "}
               </p>
               <p>
                 {" "}
-                Our North Star is <strong>resonance</strong>. Because the genius
-                in you stimulates the genius in us.
+                Our North Star is <span className="font-medium">resonance</span>
+                .<br /> Because the genius in you stimulates the genius in us.
+              </p>
+            </div>
+            <div className="slide-content show-for-mob">
+              <p>
+                {" "}
+                Established by a group of
+                <br /> expert executives <br />
+                with a remarkable record as pioneers,
+                <br /> operators, creators, artists and academics, <br />
+                LeftFront undertakes Daedalian
+                <br /> problems with a rare level of
+                <br /> proficiency, integrity + efficacy.
+              </p>
+              <p>
+                Our North Star is <span className="font-medium">resonance</span>
+                .<br /> Because the genius in you stimulates
+                <br /> the genius in us.
               </p>
             </div>
           </div>
@@ -104,28 +129,58 @@ export function SwiperComponent() {
                   </div>
                 </div>
               </div>
-              <p className="slide3-description">
-                Undertaking all 3 makes us better at each one of them.
+              <p className="slide3-description show-for-web">
+                Undertaking all 3 <br />
+                makes us better <br />
+                at each one of them.
+              </p>
+              <p className="slide3-description show-for-mob">
+                Undertaking all 3 makes us better <br />
+                at each one of them.
               </p>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide className="swiper-slide">
           <div className="slide-wrapper4">
-            <div className="slide-content">
+            <div className="slide-content show-for-web">
               <p>Publishing House.</p>
               <p>
-                With the help of creative minds around the globe, we fund Insert
-                Society Here (ISH).
+                With the help of creative minds around the globe,
+                <br /> we fund Insert Society Here (ISH).
               </p>
               <p>
                 A portal for authentic expression,
-                <br /> ISH broadcasts various forms of content created by teens.
-                <br /> All teens, maneuvering and understanding their evolving
-                world.
+                <br /> ISH broadcasts various forms of content <br /> created by
+                teens.
+                <br /> All teens, maneuvering and understanding
+                <br /> their evolving world.
               </p>
               <p>
-                <strong>For, of, about, by… their generation.</strong> <br />
+                <span className="font-medium">
+                  For, of, about, by… their generation.
+                </span>{" "}
+                <br />
+                Monologue. Dialogue. Campaign.
+              </p>
+            </div>
+            <div className="slide-content show-for-mob">
+              <p>Publishing House.</p>
+              <p>
+                With the help of creative minds
+                <br /> around the globe, we fund <br />
+                Insert Society Here (ISH).
+              </p>
+              <p>
+                A portal for authentic expression,
+                <br /> ISH broadcasts various forms of <br />
+                content <span className="font-medium">created by teens.</span>
+                <br /> All teens, maneuvering + understanding
+                <br /> their evolving world.
+              </p>
+              <p>
+                For, of, about, by… their generation.
+                <br />
                 Monologue. Dialogue. Campaign.
               </p>
             </div>
@@ -134,33 +189,63 @@ export function SwiperComponent() {
         <SwiperSlide className="swiper-slide">
           <div className="slide-wrapper5">
             {" "}
-            <div className="slide-content">
+            <div className="slide-content show-for-web">
               <p>Value Engine.</p>
               <p>
                 {" "}
-                We are a group of <strong>difference-makers,</strong>{" "}
-                high-performing individuals who have built exceptionally
-                successful companies, launched products used by millions, and
-                along the way created billions in commercial value.
+                We are a group of{" "}
+                <span className="font-medium">difference-makers, </span>
+                high-
+                <br />
+                performing individuals who have built exceptionally
+                <br />
+                successful companies, launched products used by
+                <br /> millions, and along the way created
+                <br /> billions in commercial value.
               </p>
               <p>
-                Our clients range from countless start-ups to widely known
-                brands such as <i>The New York Times,</i> Apple and creative
-                work by Annie Leibovitz.
+                Our clients range from countless start-ups
+                <br /> to widely known brands such as{" "}
+                <i>
+                  The New York <br />
+                  Times,
+                </i>{" "}
+                Apple and creative work by Annie Leibovitz.
+              </p>
+            </div>
+            <div className="slide-content show-for-mob">
+              <p>Value Engine.</p>
+              <p>
+                {" "}
+                We are a group of{" "}
+                <span className="font-medium">difference-makers, </span> <br />
+                high-performing individuals <br /> who have built exceptionally
+                successful <br />
+                companies, launched products used by
+                <br /> millions, and along the way created
+                <br /> billions in commercial value.
+              </p>
+              <p>
+                Our clients range from countless
+                <br /> start-ups to widely known brands
+                <br /> such as <i>The New York Times,</i> <br />
+                Apple and creative work
+                <br /> by Annie Leibovitz.
               </p>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide className="swiper-slide">
           <div className="slide-wrapper6">
-            <div className="slide-content">
+            <div className="slide-content show-for-web">
               <p>Community.</p>
               <p>
                 We formed and manage RIFF, Radical Imagination
                 <br /> For Future, a members only clubhouse for the next
+                <br />
                 generation of creative, media, and technology executives
                 <br /> (who happen to live
-                <strong> somewhere in California</strong>).
+                <span className="font-medium"> somewhere in California</span>).
               </p>
               <p>
                 Together, we put forward a narrative of horizons.
@@ -168,20 +253,56 @@ export function SwiperComponent() {
                 Giving rise to a life, rich with possibility and play.
               </p>
             </div>
+            <div className="slide-content show-for-mob">
+              <p>Community.</p>
+              <p>
+                We formed and manage RIFF,
+                <br /> Radical Imagination For Future,
+                <br /> a members only clubhouse for the <br />
+                next generation of creative,
+                <br /> media, and technology executives
+                <br /> (who happen to live
+                <span className="font-medium"> somewhere in California</span>).
+              </p>
+              <p>
+                Together, we put forward
+                <br /> a narrative of horizons.
+                <br /> Embracing complex subjects,
+                <br /> contextualized ideas. <br />
+                Giving rise to a life,
+                <br /> rich with possibility + play.
+              </p>
+            </div>
           </div>
         </SwiperSlide>
         <SwiperSlide className="swiper-slide">
           <div className="slide-wrapper7">
-            <div className="slide-content">
+            <div className="slide-content show-for-web">
               <p>
                 Working with LeftFront is{" "}
-                <strong> surfing for the mind.</strong> <br />
+                <span className="font-medium"> surfing for the mind.</span>{" "}
+                <br />
                 Sometimes jaw-dropping. Sometimes simple. <br />
                 Always honest.{" "}
               </p>
               <p>
-                Finally, yet importantly: we are about the human touch. Feel
-                free to connect.
+                Finally, yet importantly: we are about the human
+                <br /> touch. Feel free to connect.
+                <br /> +1–818–963–2116
+              </p>
+            </div>
+            <div className="slide-content show-for-mob">
+              <p>
+                Working with LeftFront is
+                <br />
+                <span className="font-medium"> surfing for the mind.</span>{" "}
+                <br />
+                Sometimes jaw-dropping.
+                <br /> Sometimes simple. <br />
+                Always honest.{" "}
+              </p>
+              <p>
+                Feel free to connect.
                 <br /> +1–818–963–2116
               </p>
             </div>
