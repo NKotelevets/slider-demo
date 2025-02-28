3b8dcfa1ecb8c3a68d28d14c0c041b4c8449810c
Nikolay-Kotelevets <nikolay.kotelevets@scrumlaunch.com>
2025-02-28 19:52:52 +0200

    fix distances

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     domains: ["your-domain.com"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    formats: ["image/webp", "image/avif"],
+    formats: ["image/jpeg", "image/png"],
     minimumCacheTTL: 60,
   },
 };
