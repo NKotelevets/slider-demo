b32d0a76992ae638748081e8741023219f1ce5b6
Nikolay-Kotelevets <nikolay.kotelevets@scrumlaunch.com>
2025-03-05 14:40:29 +0200

    changes

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["your-domain.com"],
+    unoptimized: true,
+    domains: ["lider-demo-8wbw.vercel.app"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     formats: [],
     minimumCacheTTL: 60,
-    unoptimized: true,
   },
 };
 
